94d2cca6393ea9b847069ae131ea5bb2f581e2cd
Vitor Stachanczyk <vitor.stachanczyk@gmail.com>
2020-05-21 15:25:33 -0300

    Consulta simples

--- src/main/java/com/lightninggames/store/resources/PostResource.java
+++ src/main/java/com/lightninggames/store/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.lightninggames.store.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lightninggames.store.domain.Post;
+import com.lightninggames.store.resources.util.URL;
 import com.lightninggames.store.services.PostService;
 
 @RestController
@@ -23,5 +26,12 @@ public class PostResource {
 	public ResponseEntity<Optional<Post>> findById(@PathVariable String id){
 		Optional<Post> obj = service.findById(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="")String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);		
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/lightninggames/store/services/PostService.java
+++ src/main/java/com/lightninggames/store/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lightninggames.store.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Jogo não encontrado");
 		}
 		return game;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
